22bc1f5992f2f2ee8713d268d257f1da7ca32ac4
Arie <Rozindzakwan@gmail.com>
2023-02-07 08:09:09 +0700

    Add project files.

---
+++ UTS_NETLanjutan_DedeAdamAlamsyah/Models/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace UTS_NETLanjutan_DedeAdamAlamsyah.Models
+{
+	public enum JudulBuku
+	{
+		Komik_One_Piece,
+		Komik_Black_Clover,
+		Komik_Jujutsu_Kaisen,
+		Novel_Andrea_Hirata,
+		Novel_JK_Rowling,
+		Novel_Tere_Liye,
+		Sejarah_Indonesia,
+		Sejarah_Yunani,
+		Sejarah_Jepang
+	}
+}
---
+++ UTS_NETLanjutan_DedeAdamAlamsyah/Models/KoleksiBuku.cs
@@ -0,0 +1,34 @@
+﻿namespace UTS_NETLanjutan_DedeAdamAlamsyah.Models
+{
+	public class KoleksiBuku
+	{
+		public JudulBuku JudulBuku { get; set; }
+
+		public KoleksiBuku(JudulBuku judulBuku)
+		{
+			JudulBuku = judulBuku;
+		}
+
+		public string GenreBuku
+		{
+			get
+			{
+				string genre = "";
+				if (JudulBuku == JudulBuku.Komik_Black_Clover || JudulBuku == JudulBuku.Komik_One_Piece || JudulBuku == JudulBuku.Komik_Jujutsu_Kaisen)
+				{
+					genre = "Komik";
+				}
+				else if (JudulBuku == JudulBuku.Novel_Andrea_Hirata || JudulBuku == JudulBuku.Novel_JK_Rowling || JudulBuku == JudulBuku.Novel_Tere_Liye)
+				{
+					genre = "Novel";
+				}
+				else if (JudulBuku == JudulBuku.Sejarah_Indonesia || JudulBuku == JudulBuku.Sejarah_Yunani || JudulBuku == JudulBuku.Sejarah_Jepang)
+				{
+					genre = "Sejarah";
+				}
+
+				return genre;
+			}
+		}
+	}
+}
